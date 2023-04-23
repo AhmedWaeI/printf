78c340b026539cdba7fe4cf1fd65bcb35d64ea65
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 15:11:14 -0700

    asd

--- _printf.c
+++ _printf.c
@@ -1,25 +1,22 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
 
 #define BUFF_SIZE 1024
 
-int printstring(char* s, int len)
+int printstring(char *s, int len)
 {
-    write(1, s, len);
-    return (0);
+    return (write(1, s, len));
 }
 
 int printchar(char c)
 {
-    write(1, &c, 1);
-    return (0);
+    return (write(1, &c, 1));
 }
 
-unsigned long int strlen(char* s)
+unsigned long int _strlen(char *s)
 {
-    int i = 0;
+    unsigned long int i = 0;
 
     while (s[i] != '\0')
     {
@@ -29,81 +26,73 @@ unsigned long int strlen(char* s)
     return (i);
 }
 
-int _printf(const char* const format, ...)
+void print_buffer(char buffer[], int *buff_ind)
 {
-    if (format == NULL) {
-        return -1;
+    if (*buff_ind > 0)
+    {
+        write(1, &buffer[0], *buff_ind);
+        *buff_ind = 0;
     }
+}
 
-    int count = 0;
-    char buffer[BUFF_SIZE];
-    int buff_ind = 0;
+int _printf(const char *format, ...)
+{
+    int i, count = 0, buff_ind = 0;
+    char c, *s;
+    int len, printed_chars = 0;
     va_list args;
+    char buffer[BUFF_SIZE];
+
+    if (format == NULL)
+        return (-1);
 
     va_start(args, format);
-
-    for (int i = 0; format[i] != '\0'; i++)
+    for (i = 0; format && format[i] != '\0'; i++)
     {
         if (format[i] == '%')
         {
-            i++;
+            print_buffer(buffer, &buff_ind);
 
-            switch (format[i])
+            switch (format[i + 1])
             {
-                case 'c':
-                {
-                    char c = va_arg(args, int);
-                    buffer[buff_ind++] = c;
-                    if (buff_ind == BUFF_SIZE) {
-                        printstring(buffer, buff_ind);
-                        buff_ind = 0;
-                    }
-                    count++;
-                    break;
-                }
-                case 's':
-                {
-                    char* s = va_arg(args, char*);
-                    int len = strlen(s);
-                    for (int j = 0; j < len; j++) {
-                        buffer[buff_ind++] = s[j];
-                        if (buff_ind == BUFF_SIZE) {
-                            printstring(buffer, buff_ind);
-                            buff_ind = 0;
-                        }
-                    }
-                    count += len;
-                    break;
-                }
-                case '%':
-                {
-                    buffer[buff_ind++] = '%';
-                    if (buff_ind == BUFF_SIZE) {
-                        printstring(buffer, buff_ind);
-                        buff_ind = 0;
-                    }
-                    count++;
-                    break;
-                }
-                default:
-                    return -1;
+            case 'c':
+                c = va_arg(args, int);
+                printed_chars += printchar(c);
+                break;
+
+            case 's':
+                s = va_arg(args, char *);
+                len = _strlen(s);
+                printed_chars += printstring(s, len);
+                break;
+
+            case '%':
+                printed_chars += printchar('%');
+                break;
+
+            default:
+                printed_chars += printchar(format[i]);
+                printed_chars += printchar(format[i + 1]);
+                break;
             }
+            i++;
         }
         else
         {
             buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE) {
-                printstring(buffer, buff_ind);
-                buff_ind = 0;
+            if (buff_ind == BUFF_SIZE)
+            {
+                print_buffer(buffer, &buff_ind);
+                printed_chars += BUFF_SIZE;
             }
-            count++;
+            else
+            {
+                printed_chars++;
+            }
         }
     }
-
-    printstring(buffer, buff_ind);
-
     va_end(args);
-
-    return count;
+    print_buffer(buffer, &buff_ind);
+    return (printed_chars);
 }
 
