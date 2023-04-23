ddf67e953cb9d5763369650d406c2d2935e3e2b0
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 15:08:14 -0700

    asd

--- _printf.c
+++ _printf.c
@@ -3,37 +3,31 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
- * sum_them_all - calculates the sum of all its parameters
- * @n: number of arguments passed to the function
- *
- * Return: the resulting sum
- */
-
-int printstring(char* s, int len)
-{
-    write(1, s, len);
-    return (0);
+int printstring(char* s, int len) {
+    if (write(1, s, len) == -1) {
+        return -1;
+    }
+    return 0;
 }
-int printchar(char c)
-{
-    write(1, &c, 1);
-    return (0);
 
+int printchar(char c) {
+    if (write(1, &c, 1) == -1) {
+        return -1;
+    }
+    return 0;
 }
-unsigned long int strlen(char* s)
-{
+
+unsigned long int strlen(char* s) {
     int i = 0;
 
-    while (s[i] != '\0')
-    {
+    while (s[i] != '\0') {
         i++;
     }
 
     return (i);
 }
-int _printf(const char* const format, ...)
-{
+
+int _printf(const char* const format, ...) {
     int i;
     int count = 0;
     char c;
@@ -42,41 +36,53 @@ int _printf(const char* const format, ...)
     va_list args;
 
     va_start(args, format);
-    if (format == NULL)
-    {
-        return (-1);
-    }
-    if (format) {
-        for (i = 0; format && format[i] != '\0'; i++)
-        {
-            if (format[i] == '%')
-            {
 
-                switch (format[i + 1])
-                {
+    if (format == NULL) {
+        return -1;
+    }
+
+    for (i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            if (format[i + 1] == '\0') {
+                return -1; // error: incomplete format specifier
+            }
+
+            switch (format[i + 1]) {
                 case 'c':
                     c = va_arg(args, int);
-                    printchar(c);
+                    if (printchar(c) == -1) {
+                        return -1; // error: failed to print char
+                    }
                     count++;
                     break;
                 case 's':
                     s = va_arg(args, char*);
                     len = strlen(s);
-                    printstring(s, len);
+                    if (printstring(s, len) == -1) {
+                        return -1; // error: failed to print string
+                    }
                     count = count + len;
                     break;
-
-
-
-                }
+                case '%':
+                    if (printchar('%') == -1) {
+                        return -1; // error: failed to print '%'
+                    }
+                    count++;
+                    break;
+                default:
+                    return -1; // error: invalid conversion specifier
             }
-              if (!((format[i] == '%' || format[i - 1] == '%') && ((format[i + 1] == 's' || format[i + 1] == 'c') || (format[i] == 's' || format[i] == 'c'))))
-            {
-                printchar(format[i]);
-                count++;
+            i++; // skip over conversion specifier character
+        } else {
+            if (printchar(format[i]) == -1) {
+                return -1; // error: failed to print character
             }
+            count++;
         }
-        va_end(args);
     }
-    return (count);
+
+    va_end(args);
+
+    return count;
 }
+
