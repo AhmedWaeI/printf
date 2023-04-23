557d22c5c3ecbc4e11a666b55ccfa68641860a03
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 13:50:58 -0700

    asd

---
+++ _printf.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
+
+int printstring(char* s, int len)
+{
+    write(1, s, len);
+    return (0);
+}
+int printchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+
+}
+unsigned long int strlen(char* s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return (i);
+}
+int _printf(const char* const format, ...)
+{
+    int i;
+    int count = 0;
+    char c;
+    char* s;
+    int len;
+    va_list args;
+
+    va_start(args, format);
+    if (format == NULL)
+    {
+        return (-1);
+    }
+    if (format) {
+        for (i = 0; format && format[i] != '\0'; i++)
+        {
+            if (format[i] == '%')
+            {
+
+                switch (format[i + 1])
+                {
+                case 'c':
+                    c = va_arg(args, int);
+                    printchar(c);
+                    count++;
+                    break;
+                case 's':
+                    s = va_arg(args, char*);
+                    len = strlen(s);
+                    printstring(s, len);
+                    count = count + len;
+                    break;
+
+
+
+                }
+            }
+            if (!((format[i] == '%' || format[i - 1] == '%') && ((format[i + 1] == 's' || format[i + 1] == 'c') || (format[i] == 's' || format[i] == 'c'))))
+            {
+                printchar(format[i]);
+                count++;
+            }
+        }
+        va_end(args);
+    }
+    return (count);
+}
