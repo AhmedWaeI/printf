aa02f85664157a21bdfd5e6c70108be51c81aa0a
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 14:29:47 -0700

    asd

--- _printf.c
+++ _printf.c
@@ -1,66 +1,82 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
  */
-int _printf(const char *format, ...)
+
+int printstring(char* s, int len)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+    write(1, s, len);
+    return (0);
 }
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
+int printchar(char c)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+    write(1, &c, 1);
+    return (0);
 
-	*buff_ind = 0;
+}
+unsigned long int strlen(char* s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return (i);
+}
+int _printf(const char* const format, ...)
+{
+    int i;
+    int count = 0;
+    char c;
+    char* s;
+    int len;
+    va_list args;
+
+    va_start(args, format);
+    if (format == NULL)
+    {
+        return (-1);
+    }
+    if (format) {
+        for (i = 0; format && format[i] != '\0'; i++)
+        {
+            if (format[i] == '%')
+            {
+
+                switch (format[i + 1])
+                {
+                case 'c':
+                    c = va_arg(args, int);
+                    printchar(c);
+                    count++;
+                    break;
+                case 's':
+                    s = va_arg(args, char*);
+                    len = strlen(s);
+                    printstring(s, len);
+                    count = count + len;
+                    break;
+
+
+
+                }
+            }
+            if (!((format[i] == '%' || format[i - 1] == '%') && ((format[i + 1] == 's' || format[i + 1] == 'c') || (format[i] == 's' || format[i] == 'c'))))
+            {
+                printchar(format[i]);
+                count++;
+            }
+        }
+        va_end(args);
+    }
+    return (count);
 }
