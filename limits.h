a36514c0b1237507c927a8528395f8f1d2d56e50
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 13:52:40 -0700

    asd

--- limits.h
+++ limits.h
@@ -4,4 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 int _printf(const char *format, ...);
+int printstring(char* s, int len);
+int printchar(char c);
+unsigned long int strlen(char* s);
 #endif
