9632148caa15de17f73c3a25db8ddcb5f6d917a8
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 14:00:37 -0700

    asd

--- _printf.c
+++ _printf.c
@@ -9,29 +9,6 @@
  *
  * Return: the resulting sum
  */
-
-int printstring(char* s, int len)
-{
-    write(1, s, len);
-    return (0);
-}
-int printchar(char c)
-{
-    write(1, &c, 1);
-    return (0);
-
-}
-unsigned long int strlen(char* s)
-{
-    int i = 0;
-
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-
-    return (i);
-}
 int _printf(const char* const format, ...)
 {
     int i;
