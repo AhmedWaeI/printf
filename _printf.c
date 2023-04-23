52595f19e9deca10180cbc78bb10e2b9479373a9
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 14:18:56 -0700

    asd

--- _printf.c
+++ _printf.c
@@ -1,123 +1,58 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
-/************************* PRINT A STRING *************************/
-/**
- * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int printstring(char* s, int len)
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
 {
-    write(STDOUT_FILENO, s, len);
-    return (0);
-}
-/************************* PRINT CHAR *************************/
-
-/**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int printchar(char c)
-{
-    write(STDOUT_FILENO, &c, 1);
-    return (0);
-
-}
-/************************* PRINT CHAR *************************/
-
-/**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-unsigned long int strlen(char* s)
-{
-    int i = 0;
-
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-
-    return (i);
-}
-/**
- * sum_them_all - calculates the sum of all its parameters
- * @n: number of arguments passed to the function
- *
- * Return: the resulting sum
- */
-int _printf(const char* const format, ...)
-{
-    int i;
-    int count = 0;
-    char c;
-    char* s;
-    int len;
     va_list args;
-
     va_start(args, format);
-    if (format == NULL)
+
+    int printed_chars = 0;
+
+    while (*format)
     {
-        return (-1);
-    }
-    if (format) {
-        for (i = 0; format && format[i] != '\0'; i++)
+        if (*format == '%')
         {
-            if (format[i] == '%')
+            // handle format specifier
+            format++;  // skip the '%'
+
+            if (*format == 'c')
             {
-
-                switch (format[i + 1])
+                // print a single character
+                int c = va_arg(args, int);
+                write(STDOUT_FILENO, &c, 1);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                // print a string
+                char *s = va_arg(args, char *);
+                while (*s)
                 {
-                case 'c':
-                    c = va_arg(args, int);
-                    printchar(c);
-                    count++;
-                    break;
-                case 's':
-                    s = va_arg(args, char*);
-                    len = strlen(s);
-                    printstring(s, len);
-                    count = count + len;
-                    break;
-
-
-
+                    write(STDOUT_FILENO, s, 1);
+                    s++;
+                    printed_chars++;
                 }
             }
-            if (!((format[i] == '%' || format[i - 1] == '%') && ((format[i + 1] == 's' || format[i + 1] == 'c') || (format[i] == 's' || format[i] == 'c'))))
+            else if (*format == '%')
             {
-                printchar(format[i]);
-                count++;
+                // print a literal '%'
+                write(STDOUT_FILENO, "%", 1);
+                printed_chars++;
             }
         }
-        va_end(args);
+        else
+        {
+            // print a literal character
+            write(STDOUT_FILENO, format, 1);
+            printed_chars++;
+        }
+
+        format++;  // advance to the next character
     }
-    return (count);
+
+    va_end(args);
+
+    return printed_chars;
 }
-int main(void)
-{
-    int len;
-    len = _printf("Let's try to printf a simple sentence.\n");
-     _printf("Character:[%c]\n", 'H');
-      _printf("String:[%s]\n", "I am a string !");
-      printf("%d",len);
-}
+
