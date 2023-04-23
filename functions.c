a14b8f6b59fa93c6eb61d6e84ec852efe415577e
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-23 13:59:06 -0700

    asd

---
+++ functions.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/************************* PRINT A STRING *************************/
+/**
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int printstring(char* s, int len)
+{
+    write(1, s, len);
+    return (0);
+}
+/************************* PRINT CHAR *************************/
+
+/**
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int printchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+
+}
+/************************* PRINT CHAR *************************/
+
+/**
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+unsigned long int strlen(char* s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return (i);
+}
