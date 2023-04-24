65fe2876d9b8d02b61614cdf5f19b5846ae277b0
xSIayer <ahmed_the_best17@yahoo.com>
2023-04-24 04:43:17 -0700

    asd

--- _printf.c
+++ _printf.c
@@ -15,10 +15,10 @@
  * Return: Number of chars printed
  */
 
-int printstring(char* s, int len)
+int printstring(char *s, int len)
 {
-    write(1, s, len);
-    return (len);
+	write(1, s, len);
+	return (len);
 }
 /************************* PRINT CHAR *************************/
 
@@ -34,9 +34,8 @@ int printstring(char* s, int len)
  */
 int printchar(char c)
 {
-    write(1, &c, 1);
-    return (1);
-
+	write(1, &c, 1);
+	return (1);
 }
 
 /**
@@ -45,105 +44,116 @@ int printchar(char c)
  *
  * Return: the length of the string
  */
-unsigned long int strlen(const char* s)
+unsigned long int strlen(const char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	return (i);
+}
+/************************* PRINT INTT *************************/
+/**
+ * print_intt - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+void print_intt(int num)
+{
+	char str[10];
+	int i = 0;
 
-    return (i);
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return;
+	}
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+	}
+	while (num > 0)
+	{
+		str[i++] = num % 10 + '0';
+		num /= 10;
+	}
+	while (i > 0)
+	{
+		write(1, &str[--i], 1);
+	}
 }
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-void print_intt(int num) {
-    char str[10];
-    int i = 0;
-    if (num == 0) {
-        write(1, "0", 1);
-        return;
-    }
-    if (num < 0) {
-        write(1, "-", 1);
-        num = -num;
-    }
-    while (num > 0) {
-        str[i++] = num % 10 + '0';
-        num /= 10;
-    }
-    while (i > 0) {
-        write(1, &str[--i], 1);
-    }
-}
-int _printf(const char* const format, ...)
+int _printf(const char *const format, ...)
 {
-    int i;
-    int x;
-    int count = 0;
-    char c;
-    char* s;
-    int len;
-    va_list args;
+	int i;
+	int x;
+	int count = 0;
+	char c;
+	char *s;
+	int len;
+	va_list args;
 
-    va_start(args, format);
-    if (format == NULL)
-    {
-        return (-1);
-    }
-    if (format) {
-        for (i = 0; format && format[i] != '\0'; i++)
-        {
-              if (format[i]== '%' && format[i+1]=='%')
-            {
-                    printchar('%');
-                    count++;
-                    i=i+2;        
-            }
-            if (format[i] == '%')
-            {
-
-                switch (format[i + 1])
-                {
-                case 'c':
-                    c = va_arg(args, int);
-                    printchar(c);
-                    count++;
-                    break;
-                case 's':
-                    s = va_arg(args, char*);
-                    len = strlen(s);
-                    printstring(s, len);
-                    count = count + len;
-		    break;
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	if (format)
+	{
+		for (i = 0; format && format[i] != '\0'; i++)
+		{
+			if (format[i]== '%' && format[i+1]=='%')
+			{
+				 printchar('%');
+				 count++;
+				 i = i + 2;
+			}
 		
-		case 'i':
-		    x = va_arg(args, int);
-		    print_intt(x);
-		    count++;
-		    break;
-		case 'd':
-		    x = va_arg(args, int);
-                    print_intt(x);
-                    count++;
-                    break;
-
-
-                }
-            }
-                if (!((format[i] == '%' || format[i - 1] == '%') && ((format[i + 1] == 's' || format[i + 1] == 'c'  || format[i + 1] == 'i' || format[i + 1] == 'd') || (format[i] == 's' || format[i] == 'c'  || format[i] == 'i' || format[i] == 'd'))))
-
-
-            {
-                printchar(format[i]);
-                count++;
-            }
-        }
-        va_end(args);
-    }
-    return (count);
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					c = va_arg(args, int);
+					printchar(c);
+					break;
+				case 's':
+					s = va_arg(args, char*);
+					len = strlen(s);
+					printstring(s, len);
+					count = count + len;
+					break;
+				case 'i':
+					x = va_arg(args, int);
+					print_intt(x);
+					count++;
+					break;
+				case 'd':
+					x = va_arg(args, int);
+					print_intt(x);
+					count++;
+					break;
+			}
+		}
+		}
+		 if (!((format[i] == '%' || format[i - 1] == '%') && ((format[i + 1] == 's' || format[i + 1] == 'c'  || format[i + 1] == 'i' || format[i + 1] == 'd') || (format[i] == 's' || format[i] == 'c'  || format[i] == 'i' || format[i] == 'd'))))
+		{
+			printchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
