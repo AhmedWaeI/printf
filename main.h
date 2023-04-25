d90d40521745c274244f35f24af33cc369d8458b
Ahmed Wael Salah <88168055+xSIayer@users.noreply.github.com>
2023-04-25 12:44:25 +0200

    Update main.h

--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
+int _printf(const char *format, ...);
 int printstring(char* s, int len);
 int printchar(char c);
 unsigned long int strllen(const char* s);
